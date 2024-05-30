ab87a934272cc1ba495330362b5881e89867368d
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-31 00:06:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 # Load image from file
 image = Image.open('dollar.png')
-new_image = image.resize((1000, 100))
+new_image = image.resize((1000, 200))
 st.image(new_image)
 
 # Display image on Streamlit app with custom height
