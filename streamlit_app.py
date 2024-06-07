3417a6ed6ac72ff3183cf2b2ef4c94f83b073cb9
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-08 03:09:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def main():
             st.markdown("<h3>Result :<span style='color:green'> 1 </span></h3>", unsafe_allow_html=True)
             
         st.markdown("<p style='color:red'> 0 = banknote is forged </p>",  unsafe_allow_html=True)
-        st.markdown("<p style='color:red'> 1 = banknote is genuine</p>", unsafe_allow_html=True)
+        st.markdown("<p style='color:green'> 1 = banknote is genuine</p>", unsafe_allow_html=True)
         st.text("Classifier : Random Forest")
         st.text("Accuracy : 99.27 %")
         st.text("Built by : Suraj R. Yadav")
