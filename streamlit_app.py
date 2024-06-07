04f8e65fddb26805efafa0c3026b599cb49f41c3
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-08 02:23:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def main():
 
     if st.button("Get Prediction"):
         output = predict_note_authentication(variance, skewness, curtosis, entropy)
-        st.success('Result: {output}')
+        st.success(f'Result: {output}')
         st.write('0 = banknote is forged')
         st.write('1 = banknote is genuine')
         st.text("Classifier : Random Forest")
