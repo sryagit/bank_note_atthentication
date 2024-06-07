80510c1c65d837b51d4e3824670afbccb21c83ce
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-08 00:58:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,20 @@
 import numpy as np
 import pandas as pd
 import joblib
-import streamlit as st 
+import streamlit as st
 from PIL import Image
 
-# model = joblib.load('classifier.joblib')
+model = joblib.load('classifier.joblib')
 
 image = Image.open('dollar.png')
 st.image(image.resize((1000, 300)))
-# st.image(image, caption=None, width=None, use_column_width=True, clamp=False, channels="RGB", output_format="auto")
 
-# new_image = image.resize((1000, 300))
-# st.image(new_image)
-
-# Display image on Streamlit app with custom height
-#st.image(image, use_column_width=True, height=50)
 
 def predict_note_authentication(variance, skewness, curtosis, entropy):
     prediction = model.predict([[variance, skewness, curtosis, entropy]])
     return prediction
 
 def main():
-    #st.title("Banknote Authentication Classifier")
     st.title("Bank Note Authentication Web APP")
     variance = st.text_input("variance", placeholder="Type Here")
     skewness = st.text_input("skewness", placeholder="Type Here")
@@ -37,8 +30,7 @@ def main():
     if st.button("About"):
         st.text("Classifier name : Random Forest")
         st.text("Accuracy Score : 99.27")
-        st.text("SRM 3rd SEM Internship Project")
-        st.text("Built by Suraj R. Yadav")
+
 
 if __name__ == '__main__':
     main()
