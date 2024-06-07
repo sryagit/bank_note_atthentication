e195ba2768cdee52a3e9e35f7225be0a8334873e
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-08 02:26:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 # Set the background color of the app
-st.set_page_config(page_title="My App", page_icon=":guardsman:", layout="wide", initial_sidebar_state="expanded", page_bg_color="green")
 
 # Add content to the app
 st.write("Hello, world!")
