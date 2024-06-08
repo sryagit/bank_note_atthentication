7f486acdaed08f36531201b72c091123ea0fb742
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-08 06:45:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,20 +22,16 @@ def main():
 
     if st.button("Get Prediction"):
         output = predict_note_authentication(variance, skewness, curtosis, entropy)
-        st.session_state['prediction'] = output
-
-    if 'prediction' in st.session_state:
-        if st.session_state['prediction'] == 0:
+        if output == 0:
             st.markdown("<h3>Result :<span style='color:red'> 0 </span></h3>", unsafe_allow_html=True)
         else:
             st.markdown("<h3>Result :<span style='color:LawnGreen'> 1 </span></h3>", unsafe_allow_html=True)
 
-    if st.button("About"):
-        st.markdown("<h5 style='color:red'> 0 <span style='color:white'> = </span> banknote is forged </h5>",  unsafe_allow_html=True)
-        st.markdown("<h5 style='color:LawnGreen'> 1 <span  style='color:White'> = </span> banknote is genuine</h5>", unsafe_allow_html=True)
-        st.text("Classifier : Random Forest")
-        st.text("Accuracy : 99.27 %")
-        st.text("Built by : Suraj R. Yadav")
+    st.markdown("<h5 style='color:red'> 0 <span style='color:white'> = </span> banknote is forged </h5>",  unsafe_allow_html=True)
+    st.markdown("<h5 style='color:LawnGreen'> 1 <span  style='color:White'> = </span> banknote is genuine</h5>", unsafe_allow_html=True)
+    st.text("Classifier : Random Forest")
+    st.text("Accuracy : 99.27 %")
+    st.text("Built by : Suraj R. Yadav")
 
 if __name__ == '__main__':
     main()
