fae730d341167d6c2e62c8c370e26f289f38a081
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-08 06:34:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import joblib
 import streamlit as st
 from PIL import Image
+from streamlit import SessionState
 
 model = joblib.load('classifier.joblib')
 
@@ -20,17 +21,21 @@ def main():
     curtosis = st.text_input("Curtosis", placeholder="Type Here")
     entropy = st.text_input("Entropy", placeholder="Type Here")
 
+    state = SessionState.get(prediction=None)
+
     if st.button("Get Prediction"):
         output = predict_note_authentication(variance, skewness, curtosis, entropy)
-        if output == 0:
-            st.markdown("<h3>Result :<span style='color:red'> 0 </span></h3>", unsafe_allow_html=True)
+        state.prediction = output
+
+    if state.prediction is not None:
+        if state.prediction == 0:
+            st.markdown("<h3>Result :<span> 0 </span></h3>", unsafe_allow_html=True)
         else:
-            st.markdown("<h3>Result :<span style='color:LawnGreen'> 1 </span></h3>", unsafe_allow_html=True)
+            st.markdown("<h3>Result :<span> 1 </span></h3>", unsafe_allow_html=True)
 
     if st.button("About"):
-        
-        st.markdown("<h5 style='color:red'> 0 <span style='color:white'> = </span> banknote is forged </h5>",  unsafe_allow_html=True)
-        st.markdown("<h5 style='color:LawnGreen'> 1 <span  style='color:White'> = </span> banknote is genuine</h5>", unsafe_allow_html=True)
+        st.markdown("<h5> 0 <span> = </span> banknote is forged </h5>",  unsafe_allow_html=True)
+        st.markdown("<h5> 1 <span> = </span> banknote is genuine</h5>", unsafe_allow_html=True)
         st.text("Classifier : Random Forest")
         st.text("Accuracy : 99.27 %")
         st.text("Built by : Suraj R. Yadav")
